2b1207f83cb96d7eb54a19f572ea516debd90762
hirahirashun <77741035+hirahirashun@users.noreply.github.com>
2021-02-02 12:23:21 +0900

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -8,12 +8,12 @@ b = 0.4 #真のb
 p = np.empty((0, 2)) #データ格納用
 for i in range(N):
 	x = random.uniform(0, 10) 
-	y = w*x + b + random.uniform(-0.8, 0.8) #δ < |0.2|の乱数付与
+	y = w*x + b + random.uniform(-0.2, 0.2) #δ < |0.2| or |0.8|の乱数付与
 	p = np.append(p, np.array([[x, y]]), axis = 0) 
 
 p = p[np.argsort(p[:, 0])] #座標の並び替え
 
-with open('data2.csv', 'w') as file:
+with open('data.csv', 'w') as file:
 	w = csv.writer(file, lineterminator = '\n')
 	w.writerow(['x', 'y'])
 	w.writerows(p)
